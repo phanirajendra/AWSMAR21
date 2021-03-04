b9c654fb3e02981f6843335adfe5496e3bff71b2
testuser1 <testuser1@ip-10-50-1-232.ec2.internal>
2021-03-04 02:19:09 +0000

    ADDED Sudo Code

--- details.sh
+++ details.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 echo "THIS SCRIPT WILL PROVIDE THE INFORMATION ABOUT LOCAL SYSTEM"
+#THIS IS A HOSTNAME
 echo $(hostname)
+
+THIS IS A LOGGED IN USER
 echo $(whoami)
+
+#THIS IS A LIST OF USERS
+USERS=$(cat /etc/passwd | cut -d ":" -f 1)
+echo $USERS
